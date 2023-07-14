64764aeb3b3bbcae079b1f8ad63c685104ac2198
Yejin <dpwls8364@naver.com>
2023-07-14 22:56:54 +0900

    init commit

---
+++ src/main/java/manito/springmanito/SpringManitoApplication.java
@@ -0,0 +1,13 @@
+package manito.springmanito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringManitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringManitoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/manito/springmanito/SpringManitoApplicationTests.java
@@ -0,0 +1,13 @@
+package manito.springmanito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringManitoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
