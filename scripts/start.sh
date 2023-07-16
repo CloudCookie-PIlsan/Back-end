3714574c109ac2305a65d9ad9b9a11f5edee2e44
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-16 12:44:10 +0900

    feat : test

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,19 +13,9 @@ TIME_NOW=$(date +%c)
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
-# 이전 프로세스 종료
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
-
-if [ -z "$CURRENT_PID" ]; then
-  echo "$TIME_NOW > 현재 실행 중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
-else
-  echo "$TIME_NOW > 실행 중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
-  kill -15 "$CURRENT_PID"
-  sleep 5 # 종료가 완료될 때까지 대기
-  echo "$TIME_NOW > 애플리케이션 종료 완료" >> $DEPLOY_LOG
-fi
-
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar "$JAR_FILE" > "$APP_LOG" 2> "$ERROR_LOG" &
+nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
