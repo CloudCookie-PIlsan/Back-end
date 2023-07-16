e28c401926d5b3e25609ed5581ad19ec82afee20
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-16 14:50:42 +0900

    feat : test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,6 +10,9 @@ ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 TIME_NOW=$(date +%c)
 
+# 실행권한 주기
+chmod +x app/scripts/deploy.sh
+
 # 빌드 파일 복사
 echo "$TIME_NOW > 빌드 파일 복사" >> $DEPLOY_LOG
 cp build/libs/*.jar $JAR_FILE
