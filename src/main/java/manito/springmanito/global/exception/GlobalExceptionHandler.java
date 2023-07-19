bc5f31460e7371bdcf30ad71f5d9a49c6392cf8a
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-20 03:35:55 +0900

    토큰 예외처리

--- src/main/java/manito/springmanito/global/exception/GlobalExceptionHandler.java
+++ src/main/java/manito/springmanito/global/exception/GlobalExceptionHandler.java
@@ -32,27 +32,4 @@ public class GlobalExceptionHandler {
             HttpStatus.BAD_REQUEST
         );
     }
-
-    @ExceptionHandler({NullPointerException.class})
-    public ResponseEntity<RestApiException> handleException(NullPointerException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage());
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.BAD_REQUEST
-        );
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<RestApiException> processValidationError(MethodArgumentNotValidException ex) {
-        String defaultMessage = ex.getBindingResult().getAllErrors().get(0).getDefaultMessage();
-        RestApiException restApiException = new RestApiException(defaultMessage);
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.BAD_REQUEST
-        );
-    }
 }
