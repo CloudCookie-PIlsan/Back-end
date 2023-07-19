b2acdcfec4349840c465fee2c9aaacdf9684eb98
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-19 15:46:20 +0900

    token

--- src/main/java/manito/springmanito/global/config/WebConfig.java
+++ src/main/java/manito/springmanito/global/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://front-end-sepia-eight.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
