328947d4680fd053da8e8950499a9a4beef022ed
siwoo1224 <96398475+siwoo1224@users.noreply.github.com>
2023-07-20 03:41:11 +0900

    토큰 예외처리

--- src/main/java/manito/springmanito/global/exception/GlobalExceptionHandler.java
+++ src/main/java/manito/springmanito/global/exception/GlobalExceptionHandler.java
@@ -10,17 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(JwtException.class)
-    public ResponseEntity<RestApiException> processValidationError(JwtException ex) {
-        RestApiException restApiException = new RestApiException(ex.getMessage());
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.INTERNAL_SERVER_ERROR
-        );
-    }
-
 
     @ExceptionHandler({IllegalArgumentException.class})
     public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
